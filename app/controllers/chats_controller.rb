e0c84d78a88cef7905023c46242cc65471205970
denialtorres <denial.torres@gmail.com>
2024-08-05 19:19:43 -0600

    form view

---
+++ app/controllers/chats_controller.rb
@@ -0,0 +1,7 @@
+class ChatsController < ApplicationController
+  def create
+    # ChatJob.perform_later(params[:message])
+
+    render turbo_stream: turbo_stream.replace("chat_form", partial: "welcome/form")
+  end
+end
